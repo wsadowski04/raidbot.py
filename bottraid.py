5e7625a6bbd1443d642255855260c60afa04f7e1
wsadowski04 <woitaszek@spoko.pl>
2026-04-07 00:14:20 +0200

    Update bottraid.py

--- bottraid.py
+++ bottraid.py
@@ -2,10 +2,10 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import asyncio
-import os # Dodane do obsługi Railway
+import os
 
-# TOKEN pobierany bezpiecznie z zakładki Variables w Railway
-TOKEN = os.getenv('MTQ5MDc1MzQxNTc2MjkzOTk5Nw.GrrhQo.ekBIKV2PetAixyOSzWznocRKo29cc492uR9vzk') 
+# TOKEN pobierany z zakładki Variables w Railway
+TOKEN = os.getenv('MTQ5MDc1MzQxNTc2MjkzOTk5Nw.GrrhQo.ekBIKV2PetAixyOSzWznocRKo29cc492uR9vzk')
 
 class RaidControlView(discord.ui.View):
     def __init__(self, text):
@@ -19,7 +19,7 @@ class RaidControlView(discord.ui.View):
             description=f"**Dołącz do nas:**\n{self.invite_link}",
             color=0x2b2d31
         )
-        embed.set_footer(text="System operacyjny • Zaproszenie aktywne")
+        embed.set_footer(text="SecretRaid System • Operacja Aktywna")
         await interaction.followup.send(embed=embed)
 
     @discord.ui.button(label="Wyślij 1x", style=discord.ButtonStyle.primary)
@@ -35,28 +35,28 @@ class RaidControlView(discord.ui.View):
             await asyncio.sleep(0.4)
         
         await interaction.followup.send(
-            "✅ Seria wysłana. Kliknij ponownie, aby kontynuować:", 
+            "✅ Seria wysłana. System gotowy:", 
             view=RaidControlView(self.text), 
             ephemeral=True
         )
 
-class SecretBot(commands.Bot):
+class SecretRaid(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         super().__init__(command_prefix="!", intents=intents)
 
     async def setup_hook(self):
         await self.tree.sync()
+        print(f"Zalogowano jako {self.user}")
 
-bot = SecretBot()
+bot = SecretRaid()
 
-@bot.tree.command(name="spam", description="Nalot z linkiem (bez powiadomień)")
+@bot.tree.command(name="spam", description="Odpala panel SecretRaid")
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 @app_commands.allowed_installs(guilds=True, users=True)
 async def spam(interaction: discord.Interaction, wiadomosc: str):
     view = RaidControlView(wiadomosc)
-    await interaction.response.send_message("⚙️ **PANEL REKLAMOWY GOTOWY**", view=view, ephemeral=True)
+    await interaction.response.send_message("⚙️ **PANEL SECRETRAID**", view=view, ephemeral=True)
 
-# Odpalanie bota
 if __name__ == "__main__":
     bot.run(TOKEN)
